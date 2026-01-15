07327452a30f575d537bf40fb270a3266ec24bd6
avdes96 <avdes96@gmail.com>
2026-01-15 23:11:23 +0000

    Parser: Added printer for ast

---
+++ app/ast/print.go
@@ -0,0 +1,38 @@
+package ast
+
+import (
+	"fmt"
+	"strings"
+)
+
+func Print(expr Expr) {
+	sb := &strings.Builder{}
+	construct(expr, sb)
+	fmt.Println(sb.String())
+}
+
+func construct(expr Expr, sb *strings.Builder) {
+	switch e := expr.(type) {
+	case *Binary:
+		parenthesise(sb, e.operator.Lexeme, e.left, e.right)
+	case *Unary:
+		parenthesise(sb, e.operator.Lexeme, e.right)
+	case *Literal:
+		if e.value == nil {
+			sb.WriteString("null")
+		} else {
+			sb.WriteString(e.String())
+		}
+	case *Grouping:
+		parenthesise(sb, "grouping", e.expression)
+	}
+}
+
+func parenthesise(sb *strings.Builder, name string, exprs ...Expr) {
+	sb.WriteString("(")
+	sb.WriteString(name)
+	for _, expr := range exprs {
+		construct(expr, sb)
+	}
+	sb.WriteString(")")
+}
